7e83c85cc79428d958c92915ebc714a950623ef1
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-03 15:11:14 +0530

    [Pranav] ADD fizz-buzz

---
+++ FizzBuzz.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.err.println("Enter a positive integer.");
+            return;
+        }
+
+        // Creating a String array to store results
+        String[] results = new String[number + 1];
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        // Print results
+        for (int i = 0; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+    }
+}
