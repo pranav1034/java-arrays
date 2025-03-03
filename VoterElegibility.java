7025799a313a88bf5e20bf2abaa2c28f808d88f5
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-03 13:56:26 +0530

    [Pranav] ADD voter-eligibility

---
+++ VoterElegibility.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VoterElegibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        // Taking input for ages
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        // Checking voting eligibility
+        for (int age : ages) {
+            if (age < 0) {
+                System.out.println("Invalid age: " + age);
+            } else if (age >= 18) {
+                System.out.println("The student with age " + age + " can vote.");
+            } else {
+                System.out.println("The student with age " + age + " cannot vote.");
+            }
+        }
+
+    }
+}
