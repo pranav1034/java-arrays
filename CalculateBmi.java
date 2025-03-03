588e026f33dfa93bc9c02801558906eca4a4d534
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-03 23:31:41 +0530

    [Pranav] ADD calculate-bmi-using-arrays

---
+++ CalculateBmi.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class CalculateBmi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input for the number of persons
+        System.out.print("Enter the number of persons: ");
+        int n = sc.nextInt();
+
+        // Arrays to store height, weight, BMI, and weight status
+        double[] height = new double[n];
+        double[] weight = new double[n];
+        double[] bmi = new double[n];
+        String[] status = new String[n];
+
+        // Taking input for weight and height
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter height(m) for person " + (i + 1) + ": ");
+            height[i] = sc.nextDouble();
+
+            System.out.print("Enter weight(kg) for person " + (i + 1) + ": ");
+            weight[i] = sc.nextDouble();
+
+            // Calculate BMI
+            bmi[i] = weight[i] / (height[i] * height[i]);
+
+            // Determine weight status
+            if (bmi[i] < 18.5)
+                status[i] = "Underweight";
+            else if (bmi[i] < 24.9)
+                status[i] = "Normal weight";
+            else if (bmi[i] < 29.9)
+                status[i] = "Overweight";
+            else
+                status[i] = "Obese";
+        }
+
+        // Print output
+        for (int i = 0; i < n; i++) {
+            System.out.println("The height(kg) , weight(m) , BMI , status of person "+(i + 1) + " is " + height[i] + ", " + weight[i] + ", " + bmi[i] + ", " + status[i]);
+        }
+
+    }
+}
